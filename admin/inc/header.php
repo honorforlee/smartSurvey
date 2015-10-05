d705de5aec1978961040e5b1143446f7bb66f59f
zied <zied@access.tn>
2015-10-05 18:52:27 +0100

    install the first page of template

---
+++ admin/inc/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
+<title>Londinium - premium responsive admin template by Eugene Kopyov</title>
+
+
+<?php require_once 'css_inc.php'; ?>
+
+
+</head>
---
+++ admin/inc/js_inc.php
@@ -0,0 +1,33 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
+
+<script type="text/javascript" src="assets/plugins/plugins/charts/sparkline.min.js"></script>
+
+<script type="text/javascript" src="assets/plugins/plugins/forms/uniform.min.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/forms/select2.min.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/forms/inputmask.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/forms/autosize.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/forms/inputlimit.min.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/forms/listbox.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/forms/multiselect.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/forms/validate.min.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/forms/tags.min.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/forms/switch.min.js"></script>
+
+<script type="text/javascript" src="assets/plugins/plugins/forms/uploader/plupload.full.min.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/forms/uploader/plupload.queue.min.js"></script>
+
+<script type="text/javascript" src="assets/plugins/plugins/forms/wysihtml5/wysihtml5.min.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/forms/wysihtml5/toolbar.js"></script>
+
+<script type="text/javascript" src="assets/plugins/plugins/interface/daterangepicker.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/interface/fancybox.min.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/interface/moment.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/interface/jgrowl.min.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/interface/datatables.min.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/interface/colorpicker.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/interface/fullcalendar.min.js"></script>
+<script type="text/javascript" src="assets/plugins/plugins/interface/timepicker.min.js"></script>
+
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/application.js"></script>
